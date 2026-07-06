9e3bc31a49bb0605b6540be47d8bbb03c0e51dea
Siddaiah M <42779832+siddaiahm@users.noreply.github.com>
2026-07-06 18:27:01 +0530

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const results=[];
+        nums.sort((a,b)=>a-b)
+        for(let i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]===nums[i-1]){
+                continue;
+            }
+            let l=i+1;
+            let r= nums.length-1;
+            while(l<r){
+                const sum = nums[i]+nums[l]+nums[r];
+                if(sum==0){
+                    results.push([nums[i],nums[l],nums[r]]);
+                    while(l<r && nums[l]===nums[l+1]){
+                       l++;
+                    }
+                    while(l<r && nums[r]===nums[r+1]){
+                        r--;
+                    }
+                    l++;
+                    r--;
+                }else if(sum<0){
+                    l++
+                }else{
+                    r--;
+                }
+            }
+        }
+        return results;
+    }
+}
